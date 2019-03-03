ddfc182a9d2e42181a1c73cb36788d5ef11dbeed
crypto-jones <integral.being@gmail.com>
2019-03-03 13:48:08 -0800

    Set up graphql query for files

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,33 +2,44 @@ import React from 'react';
 import { Link, graphql } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 import Counter from '../components/counter';
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hello World!</h1>
-    <p>Welcome {data.site.siteMetadata.author}!</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <br />
-    <Link to="/page-3/">Go to page 3</Link>
-    <br />
-    <Counter color="blue" />
+    <table>
+      <thead>
+        <tr>
+          <th>relativePath</th>
+          <th>prettySize</th>
+          <th>extension</th>
+          <th>birthTime</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.allFile.edges.map(({ node }) => (
+          <tr>
+            <td>{node.relativePath}</td>
+            <td>{node.prettySize}</td>
+            <td>{node.extension}</td>
+            <td>{node.birthTime}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </Layout>
 );
 
 export const query = graphql`
-  query FirstQuery {
-    site {
-      siteMetadata {
-        title
-        author
+  query MyFilesQuery {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
       }
     }
   }
